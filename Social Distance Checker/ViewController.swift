0edb47a9173f87d8e4f9092fa755dde83a5be6a6
Ari Wasch <ariwasch@gmail.com>
2020-03-22 11:29:07 -0400

    Initial Commit

---
+++ Social Distance Checker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Social Distance Checker
+//
+//  Created by Ari Wasch on 3/22/20.
+//  Copyright © 2020 Ari Wasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
