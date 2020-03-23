58902f84c7bf07c8805cf42ef3fcbba1d1180dad
Ari Wasch <ariwasch@gmail.com>
2020-03-22 23:33:30 -0400

    initial commit

---
+++ Social Distance Checker/Vision.swift
@@ -0,0 +1,17 @@
+//
+//  Vision.swift
+//  Social Distance Checker
+//
+//  Created by Ari Wasch on 3/22/20.
+//  Copyright © 2020 Ari Wasch. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+import Vision
+
+class Vision: ViewController {
+    
+}
+
